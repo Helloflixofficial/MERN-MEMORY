584a31cf57580b1e802f36c36910d87288488cdf
Ɓʘɓƴ <shboby71@gmail.com>
2025-03-01 23:24:15 +0530

    reset

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -4,7 +4,7 @@ import { Provider } from 'react-redux';
 import { configureStore } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
 import App from './App.jsx';
-import { reducers } from './reducers';
+import { reducers } from './reducers/index.js';
 
 const store = configureStore({
   reducer: reducers,
